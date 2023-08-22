2bf044971cf7b7d7fc4c44bedc6f9e11fe88f9e8
ignaciozhetta <zagu4988@gmail.com>
2023-08-22 17:15:50 -0300

    feat: controller of products and categories

---
+++ backend/app/Http/Controllers/Api/OrdersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Orders;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function show(Orders $orders)
+    {
+        //
+    }
+
+    public function update(Request $request, Orders $orders)
+    {
+        //
+    }
+
+    public function destroy(Orders $orders)
+    {
+        //
+    }
+}
