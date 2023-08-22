b0babd36263cfa09706175549aef3024844d960d
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-08-22 12:39:01 -0400

    feat: New table Categories with foreing keys, and new api routes

--- backend/app/Http/Controllers/CategoriesController.php
+++ backend/app/Http/Controllers/CategoriesController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Products;
+use App\Models\Categories;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
-class ProductsController extends Controller
+class CategoriesController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -35,7 +34,7 @@ class ProductsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Products $products)
+    public function show(Categories $categories)
     {
         //
     }
@@ -43,7 +42,7 @@ class ProductsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Products $products)
+    public function edit(Categories $categories)
     {
         //
     }
@@ -51,7 +50,7 @@ class ProductsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Products $products)
+    public function update(Request $request, Categories $categories)
     {
         //
     }
@@ -59,7 +58,7 @@ class ProductsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Products $products)
+    public function destroy(Categories $categories)
     {
         //
     }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Path: routes\api.php
+
+Route::get('/users', 'App\Http\Controllers\UserController@index');
+Route::post('/users', 'App\Http\Controllers\UserController@store');
+Route::get('/users/{id}', 'App\Http\Controllers\UserController@show');
+Route::put('/users/{id}', 'App\Http\Controllers\UserController@update');
+Route::delete('/users/{id}', 'App\Http\Controllers\UserController@destroy');
+
+//Products Routes
+Route::get('/products', 'App\Http\Controllers\ProductsController@index');
+Route::post('/products', 'App\Http\Controllers\ProductsController@store');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductsController@show');
+Route::put('/products/{id}', 'App\Http\Controllers\ProductsController@update');
+Route::delete('/products/{id}', 'App\Http\Controllers\ProductsController@destroy');
+
+//Orders Routes
+Route::get('/orders', 'App\Http\Controllers\OrdersController@index');
+Route::post('/orders', 'App\Http\Controllers\OrdersController@store');
+Route::get('/orders/{id}', 'App\Http\Controllers\OrdersController@show');
+Route::put('/orders/{id}', 'App\Http\Controllers\OrdersController@update');
+Route::delete('/orders/{id}', 'App\Http\Controllers\OrdersController@destroy');
+
+//OrderDetails Routes
+Route::get('/orderdetails', 'App\Http\Controllers\OrdersDetailController@index');
+Route::post('/orderdetails', 'App\Http\Controllers\OrdersDetailController@store');
+Route::get('/orderdetails/{id}', 'App\Http\Controllers\OrdersDetailController@show');
+Route::put('/orderdetails/{id}', 'App\Http\Controllers\OrdersDetailController@update');
+Route::delete('/orderdetails/{id}', 'App\Http\Controllers\OrdersDetailController@destroy');
