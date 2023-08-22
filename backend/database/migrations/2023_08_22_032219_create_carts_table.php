94ad41d5190beb385aaf1c7ebf35ae2b6e7b3e45
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-08-21 23:34:46 -0400

    Creado primer modelo backend, creado varios models y migraciones

---
+++ backend/database/migrations/2023_08_22_032219_create_carts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            //id del carrito
+            $table->id();
+            //id del usuario
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            //id del producto
+            $table->unsignedBigInteger('product_id')->nullable()->default(null);
+            $table->foreign('product_id')->references('id')->on('products');
+            
+            $table->integer('Cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
