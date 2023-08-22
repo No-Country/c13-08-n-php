99b94defd8bad3760d0966e678880108d84f209c
Eric Jonatan Brandan <98064385+brandaneric@users.noreply.github.com>
2023-08-22 18:50:31 -0300

    Add files via upload

---
+++ frontend/src/pages/combos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const combos = () => {
+  return (
+    <div>combos</div>
+  )
+}
+
+export default combos
---
+++ frontend/src/pages/panificados.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const panificados = () => {
+  return (
+    <div>panificados</div>
+  )
+}
+
+export default panificados
