b895c5ef3fc565ff2b427be21a317bcf268b9a96
ines godoy <m.inesgodoyflorio@gmail.com>
2023-08-26 13:08:00 -0300

    modificaciones para el merge

--- backend/app/Models/Locations.php
+++ backend/app/Models/Locations.php
@@ -4,20 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Locations extends Model
 {
     use HasFactory;
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 
     public function street()
     {
         return $this->belongsTo(Street::class);
     }
-
 
 }
--- backend/app/Models/Street.php
+++ backend/app/Models/Street.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class Street extends Model
 {
     use HasFactory;
-
-    public function locations()
-    {
-        return $this->hasMany(Locations::class);
-    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
 }
