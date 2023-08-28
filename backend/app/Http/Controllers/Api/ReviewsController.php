7bc585b1e2bf1cbcce9c8a5d5fa7ec001d092cb2
ignaciozhetta <zagu4988@gmail.com>
2023-08-27 21:23:24 -0300

    feat: reviews added

---
+++ backend/app/Http/Controllers/Api/ReviewsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Reviews;
+use Illuminate\Http\Request;
+
+class ReviewsController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function show(reviews $reviews)
+    {
+        //
+    }
+
+    public function update(Request $request, reviews $reviews)
+    {
+        //
+    }
+}
--- backend/app/Models/reviews.php
+++ backend/app/Models/reviews.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class reviews extends Model{
     use HasFactory;
+    
     protected $fillable = [
         'product_id',
         'user_id',
