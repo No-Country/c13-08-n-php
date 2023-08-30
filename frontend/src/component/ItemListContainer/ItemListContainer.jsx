4b190cdadc1dfea49ef4d84f87709c99e9827a97
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-08-30 17:23:50 -0400

    fix: error in itemlist container

--- frontend/src/component/ItemListContainer/ItemListContainer.jsx
+++ frontend/src/component/ItemListContainer/ItemListContainer.jsx
@@ -1,53 +1,50 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { db } from '../../Services/config';
+import ItemList from '../ItemList/ItemList';
+import './ItemListContainer.css';
 
-import { useState, useEffect } from "react";
-import ItemList from "../ItemList/ItemList";
-import { useParams } from "react-router-dom";
-import {db} from '../../Services/config'
-import { collection, getDocs, where, query } from 'firebase/firestore';
-import "./ItemListContainer.css"
-import Fotter from "../fotter/Fotter";
-
- const ItemListContainer = () => {
+const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
-  const {idCategoria} = useParams();
+  const { idCategoria } = useParams();
 
   useEffect(() => {
-    const misProductos = idCategoria ? query(collection(db, "Productos"), where("category_id","==",idCategoria)) : collection(db, "Productos");
-    
+    const misProductos = idCategoria
+      ? query(
+          collection(db, 'Productos'),
+          where('category_id', '==', idCategoria)
+        )
+      : collection(db, 'Productos');
+
     getDocs(misProductos)
-    .then(res => {
-      const nuevosProductos = res.docs.map(doc => {
-        const data = doc.data()
-        return {id:doc.id, ...data}
+      .then((res) => {
+        const nuevosProductos = res.docs.map((doc) => {
+          const data = doc.data();
+          return { id: doc.id, ...data };
+        });
+        setProductos(nuevosProductos);
       })
-      setProductos(nuevosProductos);
-    })
-    .catch(error => console.error(error))
-    
+      .catch((error) => console.error(error));
   }, [idCategoria]);
 
   // Make the backgorund darker when the modal is open
 
-
   return (
-    < >
-      <div className={show ? "blur": ""}>
+    <>
         <div>
-          <h1 className="titulo">Nuestros productos</h1>
-          <h2 className="subtitulo">Panificados y Pizzas con Harinas Organicas y de fermentacion natural.</h2>
+          <h1 className='titulo'>Nuestros productos</h1>
+          <h2 className='subtitulo'>
+            Panificados y Pizzas con Harinas Organicas y de fermentacion
+            natural.
+          </h2>
         </div>
-        <ItemList productos={productos} /><div className="masProductos"> 
-        <button>
-          Ver todos los productos
-        </button>
-      </div>
-      < Fotter />
-
-      
-      
+        <ItemList productos={productos} />
+        <div className='masProductos'>
+          <button>Ver todos los productos</button>
+        </div>
     </>
-  )
-}
+  );
+};
 
-export default ItemListContainer
-
+export default ItemListContainer;
