688d68814ce63de38970af84e2419c21db645d6f
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-08-30 17:29:15 -0400

    sintax fix itemlistcontainer

--- frontend/src/component/ItemListContainer/ItemListContainer.jsx
+++ frontend/src/component/ItemListContainer/ItemListContainer.jsx
@@ -1,51 +1,43 @@
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { db } from '../../Services/config';
-import ItemList from '../ItemList/ItemList';
-import './ItemListContainer.css';
+import ItemList from "../ItemList/ItemList";
+import "./ItemListContainer.css";
 
-const ItemListContainer = () => {
+ const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
-  const { idCategoria } = useParams();
+  const {idCategoria} = useParams();
 
   useEffect(() => {
-    const misProductos = idCategoria
-      ? query(
-          collection(db, 'Productos'),
-          where('category_id', '==', idCategoria)
-        )
-      : collection(db, 'Productos');
-
+    const misProductos = idCategoria ? query(collection(db, "Productos"), where("category_id","==",idCategoria)) : collection(db, "Productos");
+    
     getDocs(misProductos)
-      .then((res) => {
-        const nuevosProductos = res.docs.map((doc) => {
-          const data = doc.data();
-          return { id: doc.id, ...data };
-        });
-        setProductos(nuevosProductos);
+    .then(res => {
+      const nuevosProductos = res.docs.map(doc => {
+        const data = doc.data()
+        return {id:doc.id, ...data}
       })
-      .catch((error) => console.error(error));
+      setProductos(nuevosProductos);
+    })
+    .catch(error => console.error(error))
+    
   }, [idCategoria]);
 
-  // Make the backgorund darker when the modal is open
-
   return (
-    <>
+    < >
         <div>
-          <h1 className='titulo'>Nuestros productos</h1>
-          <h2 className='subtitulo'>
-            Panificados y Pizzas con Harinas Organicas y de fermentacion
-            natural.
-          </h2>
+          <h1 className="titulo">Nuestros productos</h1>
+          <h2 className="subtitulo">Panificados y Pizzas con Harinas Organicas y de fermentacion natural.</h2>
         </div>
-        
-        <ItemList productos={productos} />
-        <div className='masProductos'>
-          <button>Ver todos los productos</button>
-        </div>
+        <ItemList productos={productos} /><div className="masProductos"> 
+        <button>
+          Ver todos los productos
+        </button>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default ItemListContainer;
+export default ItemListContainer
+
