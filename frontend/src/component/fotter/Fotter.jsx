71257a0effb8fcf5a241659619513ddf601ebd91
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-08-30 16:09:09 -0400

    feat: gradient footer

--- frontend/src/component/fotter/Fotter.jsx
+++ frontend/src/component/fotter/Fotter.jsx
@@ -6,19 +6,21 @@ function Fotter() {
   return (
     <Box
       sx={{
-        position: "absolute",
-        bottom: "0px",
+        //position: "absolute",
+        //bottom: "0px",
         width: "100%",
         height: "117px",
         borderRadius: "30px 30px 0px 0px",
-        backgroundColor: "#409989",
+        // backgroundColor: "#409989",
+        background: "rgb(64,153,137)",
+        background: "linear-gradient(180deg, rgba(64,153,137,1) 0%, rgba(50,51,52,1) 100%)",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center"
       }}>
 
-      <Box sx={{ color: "white", fontWeight: "bold" ,width:"300px",marginLeft:"120px"}} >
+      <Box sx={{ color: "#F4D88F", fontWeight: "bold" ,width:"300px",marginLeft:"120px", marginBottom:"18px"}} >
         Seguinos en nuestras redes
       </Box> 
       <Box sx ={{display: "flex", flexDirection:"wrap"}}>
