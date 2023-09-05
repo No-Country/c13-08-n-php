ada66478c3b8abb2a08a23f5fd189e780746d621
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-09-04 21:33:37 -0400

    fix: error for cart detail

--- frontend/src/component/CartGrid/CartGrid.jsx
+++ frontend/src/component/CartGrid/CartGrid.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import './styles.css'
 
 const CartGrid = () => {
+
+    //Function to prevent the default behavior of the form
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Formulario enviado');
+        //Aca deberia ir el codigo para enviar los datos al backend
+        const data = {
+            nombre: e.target.nombre.value,
+            direccion: e.target.direccion.value,
+            ciudad: e.target.ciudad.value,
+            provincia: e.target.provincia.value,
+            codigoPostal: e.target.codigoPostal.value,
+            telefono: e.target.telefono.value,
+            }
+            console.log(data);
+    }
+
+
   return (
     <div className='cart-container'>
 
@@ -13,7 +31,7 @@ const CartGrid = () => {
             <div className='direcciones-guardadas'>
                 <div className='direccion'>
                     <input type="radio" name='address-1' id='ad1' value={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quis."}/>
-                    <label for="ad1"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quis.</p></label>
+                    <label htmlFor="ad1"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quis.</p></label>
                 </div>
             </div>
 
@@ -22,14 +40,14 @@ const CartGrid = () => {
 
         <div className='direccion-nueva'>
             <h4>Completa con tus datos para el envío</h4>
-            <form action="" className='completar-direccion'>
-                <input type="text" placeholder='Nombre y Apellido'/>
-                <input type="text" placeholder='Dirección'/>
-                <input type="text" placeholder='Ciudad'/>
-                <input type="text" placeholder='Provincia'/>
-                <input type="text" placeholder='Código Postal'/>
-                <input type="text" placeholder='Teléfono'/>
-                <button className='checkout-button'>Continuar al pago</button>
+            <form onSubmit={handleSubmit} className='completar-direccion'>
+                <input type="text" placeholder='Nombre y Apellido' name='nombre'/>
+                <input type="text" placeholder='Dirección' name='direccion'/>
+                <input type="text" placeholder='Ciudad' name='ciudad'/>
+                <input type="text" placeholder='Provincia' name='provincia'/>
+                <input type="text" placeholder='Código Postal' name='codigoPostal'/>
+                <input type="text" placeholder='Teléfono' name='telefono'/>
+                <button type='submit' className='checkout-button'>Continuar al pago</button>
             </form>
         </div>
 
