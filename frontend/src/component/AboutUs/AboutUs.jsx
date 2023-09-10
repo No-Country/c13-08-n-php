8ee6892a2ed780a805e30dd478d7231996e38df1
Jesus <albertovasquezr@protonmail.com>
2023-09-10 03:59:35 -0600

    update AboutUS

--- frontend/src/component/AboutUs/AboutUs.jsx
+++ frontend/src/component/AboutUs/AboutUs.jsx
@@ -4,9 +4,279 @@ import "./AboutUs.css";
 
 
 const AboutUs = () => {
-  return (
-    <div>AboutUs</div>
-  )
+    let message = `Colaboradores`
+    return (
+        <section className="section-white">
+
+            <div className="container">
+
+                <div className="col-md-12 text-center">
+                    <h2 className="section-title">
+                        Equipo c13-08-n-php
+                    </h2>
+                    <p className="section-subtitle">{message}</p>
+                    <h3 className="section-departament">Departamento de UX</h3>
+                </div>
+
+                <div className="row">
+
+
+
+                    {/* Primera card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U05MYECNB4G-4c8f629ebbc8-512" className="team-img" alt="pic" />
+                            <h3>Carolina Flotts</h3>
+                            <div className="team-info">
+                                <p>Diseñadora UX</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+                <div className="col-md-12 text-center">
+                    <h3 className="section-departament">Departamento de FrontEnd</h3>
+                </div>
+
+                <div className="row">
+
+                    {/* Primera card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U05KE2LFQDD-8d6df2c1e067-512" className="team-img" alt="pic" />
+                            <h3>Jesus Ruiz</h3>
+                            <div className="team-info">
+                                <p>Desarrollador FrontEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Otra card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U058F07JNGM-bc23040b3726-512" className="team-img" alt="pic" />
+                            <h3>Gabriel Mendoza</h3>
+                            <div className="team-info">
+                                <p>Desarrollador FrontEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Otra card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U059468NHCH-6f5e2d22953c-512" className="team-img" alt="pic" />
+                            <h3>Diego Omar Bufalari</h3>
+                            <div className="team-info">
+                                <p>Desarrollador FrontEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Otra card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U05M24NV9LN-d73bbae82de3-512" className="team-img" alt="pic" />
+                            <h3>Eric Brandan</h3>
+                            <div className="team-info">
+                                <p>Desarrollador FrontEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-12 text-center">
+                    <h3 className="section-departament">Departamento de BackEnd</h3>
+                </div>
+
+                <div className="row">
+
+                    {/* Primera card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U05KN05NE2V-0c07e79d8667-512" className="team-img" alt="pic" />
+                            <h3>Maria Ines Godoy</h3>
+                            <div className="team-info">
+                                <p>Desarrolladora BackEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Otra card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U058F07JNGM-bc23040b3726-512" className="team-img" alt="pic" />
+                            <h3>Gabriel Mendoza</h3>
+                            <div className="team-info">
+                                <p>Desarrollador BackEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Otra card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U054BARC4KY-239641bad062-512" className="team-img" alt="pic" />
+                            <h3>Ignacio Zolorza</h3>
+                            <div className="team-info">
+                                <p>Desarrollador BackEnd</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                </div>
+
+                <div className="col-md-12 text-center">
+                    <h3 className="section-departament">Departamento de QA</h3>
+                </div>
+
+                <div className="row">
+
+
+
+                    {/* Primera card */}
+
+                    <div className="col-sm-6 col-md-4">
+                        <div className="team-item">
+
+                            <img src="https://ca.slack-edge.com/T02KS88FB0E-U0565C8R42Z-ga37b078b065-512" className="team-img" alt="pic" />
+                            <h3>Lucas Dipasquale</h3>
+                            <div className="team-info">
+                                <p>QA Tester Manual</p>
+                                {/* <p>texto de abajo</p> */}
+                                <ul className="team-icon">
+                                    <li><a href="#" className="linkedin">
+                                        <i className="ico-linkedin"></i>
+                                    </a></li>
+                                    <li><a href="#" className="github">
+                                        <i className="ico-github"></i>
+                                    </a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
+
+            </div>
+        </section>
+    );
 };
 
 export default AboutUs
